27f1766a2f554ae0c08c075c4f266ecf2d05bed0
tiwlxp5-glitch <tiwlxp5@gmail.com>
2026-09-17 19:27:47 +0700

    fix: resolve TS error in manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -14,13 +14,11 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/icon-192.png',
         sizes: '192x192',
         type: 'image/png',
-        purpose: 'any maskable'
       },
       {
         src: '/icon-512.png',
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'any maskable'
       }
     ],
   }
