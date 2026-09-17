bf738c5fa1b5ff115992171a0960857bdeacc2a2
tiwlxp5-glitch <tiwlxp5@gmail.com>
2026-09-17 19:24:25 +0700

    fix: resize PWA icons and update manifest

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,14 +11,16 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#38BDF8',
     icons: [
       {
-        src: '/app-icon.png',
+        src: '/icon-192.png',
         sizes: '192x192',
         type: 'image/png',
+        purpose: 'any maskable'
       },
       {
-        src: '/app-icon.png',
+        src: '/icon-512.png',
         sizes: '512x512',
         type: 'image/png',
+        purpose: 'any maskable'
       }
     ],
   }
