fb8184690db6cc13e45b1259356db78fc1496fef
tiwlxp5-glitch <tiwlxp5@gmail.com>
2026-09-17 19:29:52 +0700

    feat: add dummy service worker to satisfy chrome pwa install requirements

---
+++ src/components/PWA.tsx
@@ -0,0 +1,14 @@
+﻿'use client';
+
+import { useEffect } from 'react';
+
+export default function PWA() {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js').catch(console.error);
+      });
+    }
+  }, []);
+  return null;
+}
